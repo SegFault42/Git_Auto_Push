b022030cb06c97f13192d8d39e3b35f85bc155b7
SegFault42 <rabougue@student.42.fr>
2016-04-14 15:17:37 +0200

    Supression des fichiers '.DS_Store, ._.DS_Store' automatique

--- .push.sh
+++ .push.sh
@@ -12,16 +12,24 @@
 # 2eme cas d'utilisation (si vous utiliser zsh et que vous voulez lancer le script par une commande depuis n'importe quelle dossier):
 # Placer le script dans votre home directory (~/)
 # Editer votre fichier .zshrc qui ce trouve dans votre home directory et ajouter la ligne suivante a la fin du fichier :
-# alias push="sh ~/.push.sh"
+# alias push="sh ~/push.sh"
 
 
 if [ -d "./.git" ];then
 clear
-echo "\033[32m  ____ _ _     _         _        ____            _  __     ___   ___   ___\033[0m"
-echo "\033[32m / ___(_) |_  / \  _   _| |_ ___ |  _ \ _   _ ___| |_\ \   / / | / _ \ / _ \ \033[0m"
-echo "\033[32m| |  _| | __|/ _ \| | | | __/ _ \| |_) | | | / __| '_ \ \ / /| || | | | | | |\033[0m"
-echo "\033[32m| |_| | | |_| ___ \ |_| | || (_) |  __/| |_| \__ \ | | \ V / | || |_| | |_| |\033[0m"
-echo "\033[32m \____|_|\__|_/   \_\__,_|\__\___/|_|    \__,_|___/_| |_|\_/  |_(_)___/ \___/\033[0m"
+echo "\033[32m  ____ _ _     _         _        ____            _  __     ___   ___  _\033[0m"
+echo "\033[32m / ___(_) |_  / \  _   _| |_ ___ |  _ \ _   _ ___| |_\ \   / / | / _ \| |\033[0m"
+echo "\033[32m| |  _| | __|/ _ \| | | | __/ _ \| |_) | | | / __| '_ \ \ / /| || | | | |\033[0m"
+echo "\033[32m| |_| | | |_| ___ \ |_| | || (_) |  __/| |_| \__ \ | | \ V / | || |_| | |\033[0m"
+echo "\033[32m \____|_|\__|_/   \_\__,_|\__\___/|_|    \__,_|___/_| |_|\_/  |_(_)___|_|\033[0m"
+
+if [ "find ./ -name .DS_Store" ];then
+	find ./ -name .DS_Store -delete
+fi
+
+if [ "find ./ -name ._.DS_Store" ];then
+	find ./ -name ._.DS_Store -delete
+fi
 
 echo "\033[41mAppuyer sur Entrée pour git status...\033[0m"
 read a
