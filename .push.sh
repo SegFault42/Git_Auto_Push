65e286984d722b40c77e662a239a52b871795ca5
Ramzi BOUGUERRA <rabougue@e3r13p13.42.fr>
2017-02-17 14:28:08 +0100

    push from any folder ok

--- .push.sh
+++ .push.sh
@@ -15,7 +15,7 @@
 # alias push="sh ~/push.sh"
 
 
-if [ -d "./.git" ];then
+#if [ -d "./.git" ];then
 clear
 echo "\033[32m  _____ _ _                 _        _____           _ __      ____   ___ ___  \033[0m"
 echo "\033[32m / ____(_) |     /\        | |      |  __ \         | |\ \    / /_ | / _ \__ \ \033[0m"
@@ -23,25 +23,6 @@ echo "\033[32m| |  __ _| |_   /  \  _   _| |_ ___ | |__) |   _ ___| |_\ \  / / |
 echo "\033[32m| | |_ | | __| / /\ \| | | | __/ _ \|  ___/ | | / __| '_ \ \/ /  | || | | |/ / \033[0m"
 echo "\033[32m| |__| | | |_ / ____ \ |_| | || (_) | |   | |_| \__ \ | | \  /   | || |_| / /_ \033[0m"
 echo "\033[32m \_____|_|\__/_/    \_\__,_|\__\___/|_|    \__,_|___/_| |_|\/    |_(_)___/____|\033[0m"
-
-if [ "find ./ -name .DS_Store" ];then
-	find ./ -name .DS_Store -delete
-fi
-
-if [ "find ./ -name ._.DS_Store" ];then
-	find ./ -name ._.DS_Store -delete
-fi
-
-if [ $# -ge 1 ];then
-	for ((i = 0; i <= $1; i++));
-	do
-		find . -type d -depth $i -exec make -C {} fclean 2>/dev/null \;
-	done
-else
-	if [ "find ./ -name *.o" ];then
-		make fclean 2>/dev/null
-	fi
-fi
 
 echo "\033[41mAppuyer sur Entrée pour git status...\033[0m"
 read a
@@ -53,7 +34,7 @@ read a
 
 clear
 echo "\033[35mgit add\033[0m"
-git add *
+git add .
 echo "\033[35mgit status\033[0m"
 git status
 echo "\033[41mAppuyer sur Entrée pour git commit...\033[0m"
@@ -71,9 +52,9 @@ clear
 echo "\033[35mgit push\033[0m"
 git push
 git log
-else
-echo "\033[31mVeuillez executer le script depuis le repertoire ou ce trouve le dossier .git\033[0m"
-fi
+#else
+#echo "\033[31mVeuillez executer le script depuis le repertoire ou ce trouve le dossier .git\033[0m"
+#fi
 
 
 
